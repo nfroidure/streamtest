05390becdda9773ea6dd3ad28715ba467a731ca6
Nicolas Froidure <nicolas.froidure@gmail.com>
2024-07-17 22:32:32 +0200

    fix(core): fix for errored streams
    The stream weren't ended correctly. Added regression tests too.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,118 +1,159 @@
-import {
-  describe,
-  test,
-  expect,
-} from '@jest/globals';
+import { describe, test, expect } from '@jest/globals';
 import StreamTest from './index.js';
 
 describe('StreamTest', () => {
-      test('should work with buffers', async () => {
-        const expectedBuffers = [Buffer.from('test'), Buffer.from('test2')];
-        const inputStream = StreamTest.fromChunks(expectedBuffers);
-        const [outputStream, resultPromise] = StreamTest.toChunks();
+  test('should work with buffers', async () => {
+    const expectedBuffers = [Buffer.from('test'), Buffer.from('test2')];
+    const inputStream = StreamTest.fromChunks(expectedBuffers);
+    const [outputStream, resultPromise] = StreamTest.toChunks();
 
-        inputStream.pipe(outputStream);
+    inputStream.pipe(outputStream);
 
-        expect(await resultPromise).toEqual(expectedBuffers);
-      });
+    expect(await resultPromise).toEqual(expectedBuffers);
+  });
 
-      test('should report errors with buffers', async () => {
-        const expectedBuffers = [Buffer.from('test'), Buffer.from('test2')];
-        const expectedError = new Error('Ooops');
-        const inputStream = StreamTest.fromErroredChunks(
-          expectedError,
-          expectedBuffers
-        );
-        const [outputStream, resultPromise] = StreamTest.toChunks();
-        
-        inputStream.pipe(outputStream);
-        inputStream.on('error', (err) => {
-          outputStream.emit('error', err);
-        });
+  test('should report errors with buffers', async () => {
+    const expectedBuffers = [Buffer.from('test'), Buffer.from('test2')];
+    const expectedError = new Error('E_ERROR');
+    const inputStream = StreamTest.fromErroredChunks(
+      expectedError,
+      expectedBuffers,
+    );
+    const [outputStream, resultPromise] = StreamTest.toChunks();
 
-        try {
-          await resultPromise;
-          throw new Error('UNEXPECTED_SUCCESS');
-        } catch (err ) {
-          expect(err).toEqual(expectedError);
-        }
-      });
+    inputStream.pipe(outputStream);
+    inputStream.on('error', (err) => {
+      outputStream.emit('error', err);
+    });
 
-      test('should work when wanting whole text', async () => {
-        const expectedBuffers = ['test', 'test2'];
-        const inputStream = StreamTest.fromObjects(expectedBuffers);
-        const [outputStream, resultPromise] = StreamTest.toText();
-        
-        inputStream.pipe(outputStream);
+    try {
+      await resultPromise;
+      throw new Error('E_UNEXPECTED_SUCCESS');
+    } catch (err) {
+      expect(err).toEqual(expectedError);
+    }
+  });
 
-        expect(await resultPromise).toEqual(expectedBuffers.join(''));
-      });
+  test('should report errors and end with buffers', async () => {
+    const expectedBuffers = [Buffer.from('test'), Buffer.from('test2')];
+    const expectedError = new Error('E_ERROR');
+    const inputStream = StreamTest.fromErroredChunks(
+      expectedError,
+      expectedBuffers,
+    );
+    const [outputStream, resultPromise] = StreamTest.toChunks();
+    let _err;
 
-      test('should report errors when wanting whole text', async () => {
-        const expectedBuffers = [Buffer.from('test'), Buffer.from('test2')];
-        const expectedError = new Error('Ooops');
-        const inputStream = StreamTest.fromErroredChunks(
-          expectedError,
-          expectedBuffers
-        );
-        const [outputStream, resultPromise] = StreamTest.toText();
-        
-        inputStream.pipe(outputStream);
-        inputStream.on('error', (err) => {
-          outputStream.emit('error', err);
-        });
+    inputStream.pipe(outputStream);
+    inputStream.on('error', (err) => {
+      _err = err;
+    });
 
-        try {
-          await resultPromise;
-          throw new Error('UNEXPECTED_SUCCESS');
-        } catch (err ) {
-          expect(err).toEqual(expectedError);
-        }
-      });
+    await resultPromise;
+    expect(_err).toEqual(expectedError);
+  });
 
-      test('should work with objects', async () => {
-        const expectedObjs = [
-          {
-            test: 'test',
-          },
-          {
-            test: 'test2',
-          },
-        ];
-        const inputStream = StreamTest.fromObjects(expectedObjs);
-        const [outputStream, resultPromise] = StreamTest.toObjects();
+  test('should work when wanting whole text', async () => {
+    const expectedBuffers = ['test', 'test2'];
+    const inputStream = StreamTest.fromObjects(expectedBuffers);
+    const [outputStream, resultPromise] = StreamTest.toText();
 
-        inputStream.pipe(outputStream);
-        
-        expect(await resultPromise).toEqual(expectedObjs);
-      });
+    inputStream.pipe(outputStream);
 
-      test('should report errors with objects', async () => {
-        const expectedObjs = [
-          {
-            test: 'test',
-          },
-          {
-            test: 'test2',
-          },
-        ];
-        const expectedError = new Error('Ooops');
-        const inputStream = StreamTest.fromErroredObjects(
-          expectedError,
-          expectedObjs
-        );
-        const [outputStream, resultPromise] = StreamTest.toObjects();
-        
-        inputStream.pipe(outputStream);
-        inputStream.on('error', (err) => {
-          outputStream.emit('error', err);
-        });
+    expect(await resultPromise).toEqual(expectedBuffers.join(''));
+  });
 
-        try {
-          await resultPromise;
-          throw new Error('UNEXPECTED_SUCCESS');
-        } catch (err ) {
-          expect(err).toEqual(expectedError);
-        }
-      });
+  test('should report errors when wanting whole text', async () => {
+    const expectedBuffers = [Buffer.from('test'), Buffer.from('test2')];
+    const expectedError = new Error('E_ERROR');
+    const inputStream = StreamTest.fromErroredChunks(
+      expectedError,
+      expectedBuffers,
+    );
+    const [outputStream, resultPromise] = StreamTest.toText();
+
+    inputStream.pipe(outputStream);
+    inputStream.on('error', (err) => {
+      outputStream.emit('error', err);
+    });
+
+    try {
+      await resultPromise;
+      throw new Error('E_UNEXPECTED_SUCCESS');
+    } catch (err) {
+      expect(err).toEqual(expectedError);
+    }
+  });
+
+  test('should work with objects', async () => {
+    const expectedObjs = [
+      {
+        test: 'test',
+      },
+      {
+        test: 'test2',
+      },
+    ];
+    const inputStream = StreamTest.fromObjects(expectedObjs);
+    const [outputStream, resultPromise] = StreamTest.toObjects();
+
+    inputStream.pipe(outputStream);
+
+    expect(await resultPromise).toEqual(expectedObjs);
+  });
+
+  test('should report errors with objects', async () => {
+    const expectedObjs = [
+      {
+        test: 'test',
+      },
+      {
+        test: 'test2',
+      },
+    ];
+    const expectedError = new Error('E_ERROR');
+    const inputStream = StreamTest.fromErroredObjects(
+      expectedError,
+      expectedObjs,
+    );
+    const [outputStream, resultPromise] = StreamTest.toObjects();
+
+    inputStream.pipe(outputStream);
+    inputStream.on('error', (err) => {
+      outputStream.emit('error', err);
+    });
+
+    try {
+      await resultPromise;
+      throw new Error('E_UNEXPECTED_SUCCESS');
+    } catch (err) {
+      expect(err).toEqual(expectedError);
+    }
+  });
+
+  test('should report errors with objects', async () => {
+    const expectedObjs = [
+      {
+        test: 'test',
+      },
+      {
+        test: 'test2',
+      },
+    ];
+    const expectedError = new Error('E_ERROR');
+    const inputStream = StreamTest.fromErroredObjects(
+      expectedError,
+      expectedObjs,
+    );
+    const [outputStream, resultPromise] = StreamTest.toObjects();
+    let _err;
+
+    inputStream.pipe(outputStream);
+    inputStream.on('error', (err) => {
+      _err = err;
+    });
+
+    await resultPromise;
+    expect(_err).toEqual(expectedError);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -11,195 +11,200 @@ const StreamTest = {
    * @param timeout number
    * @returns Readable
    */
-    fromObjects: function fromObjects<T>(objects: T[] = [], timeout: number = 0): Readable {
-      const stream = new Readable({ objectMode: true });
-      const objectsLeft = objects.slice();
+  fromObjects: function fromObjects<T>(
+    objects: T[] = [],
+    timeout: number = 0,
+  ): Readable {
+    const stream = new Readable({ objectMode: true });
+    const objectsLeft = objects.slice();
 
-      stream._read = () => {
-        if (objectsLeft.length) {
-          const object: T = objectsLeft.shift() as T;
+    stream._read = () => {
+      if (objectsLeft.length) {
+        const object: T = objectsLeft.shift() as T;
 
-          setTimeout(() => {
-            stream.push(object);
-          }, timeout);
-        } else {
+        setTimeout(() => {
+          stream.push(object);
+        }, timeout);
+      } else {
+        setTimeout(() => {
+          stream.push(null);
+        }, timeout);
+      }
+    };
+    return stream;
+  },
+  /**
+   * Create a readable stream streaming `objects` each
+   *  `timeout` milliseconds, emit the `err` error and
+   *  then end. Usefull for testing `objectMode` based
+   *  streams.
+   * @function
+   * @param err Error
+   * @param objects Object[]
+   * @param timeout number
+   * @returns Readable
+   */
+  fromErroredObjects: function fromErroredObjects<T>(
+    err: Error,
+    objects: T[] = [],
+    timeout = 0,
+  ): Readable {
+    const stream = new Readable({ objectMode: true });
+    const objectsLeft = objects.slice();
+
+    stream._read = () => {
+      if (objectsLeft.length) {
+        const object: T = objectsLeft.shift() as T;
+
+        setTimeout(() => {
+          stream.push(object);
+        }, timeout);
+      } else {
+        setTimeout(() => {
+          stream.emit('error', err);
           setTimeout(() => {
             stream.push(null);
           }, timeout);
-        }
-      };
-      return stream;
-    },
-    /**
-     * Create a readable stream streaming `objects` each
-     *  `timeout` milliseconds, emit the `err` error and
-     *  then end. Usefull for testing `objectMode` based
-     *  streams.
-     * @function
-     * @param err Error
-     * @param objects Object[]
-     * @param timeout number
-     * @returns Readable
-     */
-    fromErroredObjects: function fromErroredObjects<T>(err: Error, objects: T[] = [], timeout = 0): Readable {
-      const stream = new Readable({ objectMode: true });
-      const objectsLeft = objects.slice();
-      let emitted = false;
+        }, timeout);
+      }
+    };
+    return stream;
+  },
+  /**
+   * Create a readable stream streaming `chunks` each
+   *  `timeout` milliseconds and then end. Usefull for
+   *  testing buffer based streams.
+   * @function
+   * @param chunks Buffer[]
+   * @param timeout number
+   * @returns Readable
+   */
+  fromChunks: function fromChunks(
+    chunks: Buffer[] = [],
+    timeout: number = 0,
+  ): Readable {
+    const stream = new Readable();
+    const chunksLeft = chunks.slice();
 
-      stream._read = () => {
-        
-        if (objectsLeft.length) {
-          const object:T = objectsLeft.shift() as T;
+    stream._read = () => {
+      if (chunksLeft.length) {
+        const chunk: Buffer = chunksLeft.shift() as Buffer;
 
-          setTimeout(() => {
-            stream.push(object);
-          }, timeout);
-        } else if(!emitted) {
-          setTimeout(() => {
-            stream.emit('error', err);
-          }, timeout);
-          emitted = true;
-        } else {
+        setTimeout(() => {
+          stream.push(chunk);
+        }, timeout);
+      } else {
+        setTimeout(() => {
+          stream.push(null);
+        }, timeout);
+      }
+    };
+    return stream;
+  },
+  /**
+   * Create a readable stream streaming `chunks` each
+   *  `timeout` milliseconds, emit the `err` error and
+   *  then end. Usefull for testing buffer based streams.
+   * @function
+   * @param err Error
+   * @param objects Object[]
+   * @param timeout number
+   * @returns Readable
+   */
+  fromErroredChunks: function fromErroredChunks(
+    err: Error,
+    chunks: Buffer[] = [],
+    timeout: number = 0,
+  ) {
+    const stream = new Readable();
+    const chunksLeft = chunks.slice();
+
+    stream._read = () => {
+      if (chunksLeft.length) {
+        const chunk: Buffer = chunksLeft.shift() as Buffer;
+
+        setTimeout(() => {
+          stream.push(chunk);
+        }, timeout);
+      } else {
+        setTimeout(() => {
+          stream.emit('error', err);
           setTimeout(() => {
             stream.push(null);
           }, timeout);
-        }
+        }, timeout);
+      }
+    };
+    return stream;
+  },
+  /**
+   * Create a writable stream collecting written `objects`
+   *  and a promise that resolves when it finishes with
+   *  the objects collected.
+   * @function
+   * @returns [Writable, Promise<Object>]
+   */
+  toObjects: function toObjects<T>(): [Writable, Promise<T[]>] {
+    const stream = new Writable({ objectMode: true });
+    const promise = new Promise<T[]>((resolve, reject) => {
+      const objs: T[] = [];
+
+      stream._write = (obj: T, _unused, done) => {
+        objs.push(obj);
+        done();
       };
-      return stream;
-    },
-    /**
-     * Create a readable stream streaming `chunks` each
-     *  `timeout` milliseconds and then end. Usefull for
-     *  testing buffer based streams.
-     * @function
-     * @param chunks Buffer[]
-     * @param timeout number
-     * @returns Readable
-     */
-    fromChunks: function fromChunks(chunks: Buffer[] = [], timeout: number = 0):Readable {
-      const stream = new Readable();
-      const chunksLeft = chunks.slice();
-
-      stream._read = () => {
-        if (chunksLeft.length) {
-          const chunk: Buffer = chunksLeft.shift() as Buffer;
-
-          setTimeout(() => {
-            stream.push(chunk);
-          }, timeout);
-        } else {
-          setTimeout(() => {
-            stream.push(null);
-          }, timeout);
-        }
-      };
-      return stream;
-    },
-    /**
-     * Create a readable stream streaming `chunks` each
-     *  `timeout` milliseconds, emit the `err` error and
-     *  then end. Usefull for testing buffer based streams.
-     * @function
-     * @param err Error
-     * @param objects Object[]
-     * @param timeout number
-     * @returns Readable
-     */
-    fromErroredChunks: function fromErroredChunks(err: Error, chunks: Buffer[] = [], timeout: number = 0) {
-      const stream = new Readable();
-      const chunksLeft = chunks.slice();
-      let emitted = false;
-
-      stream._read = () => {
-        if (chunksLeft.length) {
-          const chunk: Buffer = chunksLeft.shift() as Buffer;
-
-          setTimeout(() => {
-            stream.push(chunk);
-          }, timeout);
-        } else if(!emitted) {
-          setTimeout(() => {
-            stream.emit('error', err);
-          }, timeout);
-          emitted = true;
-        } else {
-          setTimeout(() => {
-            stream.push(null);
-          }, timeout);
-        }
-      };
-      return stream;
-    },
-    /**
-     * Create a writable stream collecting written `objects`
-     *  and a promise that resolves when it finishes with
-     *  the objects collected.
-     * @function
-     * @returns [Writable, Promise<Object>]
-     */
-    toObjects: function toObjects<T>(): [Writable, Promise<T[]>] {
-      const stream = new Writable({ objectMode: true });
-      const promise = new Promise<T[]>((resolve, reject) => {
-        const objs: T[] = [];
-
-        stream._write = (obj: T, _unused, done) => {
-          objs.push(obj);
-          done();
-        };
-        stream.on('error', (err) => {
-          reject(err);
-        });
-        stream.on('finish', () => {
-          resolve(objs);
-        });
+      stream.on('error', (err) => {
+        reject(err);
       });
-  
-      return [stream, promise];
-    },
-    /**
-     * Create a writable stream collecting written `chunks`
-     *  and a promise that resolves when it finishes with
-     *  the chunks collected.
-     * @function
-     * @returns [Writable, Promise<Buffer[]>]
-     */
-    toChunks: function toChunks(): [Writable, Promise<Buffer[]>] {
-      const stream = new Writable();
-        const promise = new Promise<Buffer[]>((resolve, reject) => {
-          const chunks: Buffer[] = [];
+      stream.on('finish', () => {
+        resolve(objs);
+      });
+    });
 
-          stream._write = (chunk: Buffer, encoding, done) => {
-            if (encoding === 'binary') {
-              chunks.push(chunk);
-            } else {
-              chunks.push(Buffer.alloc(chunk.length, chunk, encoding));
-            }
-            done();
-          };
-          stream.on('finish', () => {
-            resolve(chunks);
-          });
-          stream.on('error', (err) => {
-            reject(err);
-          });
-        });
-    
-        return [stream, promise];
-    },
-    /**
-     * Create a writable stream collecting written text
-     *  and a promise that resolves when it finishes with
-     *  the whole text content.
-     * @function
-     * @returns [Writable, Promise<string>]
-     */
-    toText: function toText(): [Writable, Promise<string>] {
-      const [stream, promise] = StreamTest.toChunks();
+    return [stream, promise];
+  },
+  /**
+   * Create a writable stream collecting written `chunks`
+   *  and a promise that resolves when it finishes with
+   *  the chunks collected.
+   * @function
+   * @returns [Writable, Promise<Buffer[]>]
+   */
+  toChunks: function toChunks(): [Writable, Promise<Buffer[]>] {
+    const stream = new Writable();
+    const promise = new Promise<Buffer[]>((resolve, reject) => {
+      const chunks: Buffer[] = [];
 
-      return [stream, promise.then(chunks => 
-        Buffer.concat(chunks).toString()
-      )]
-    },
+      stream._write = (chunk: Buffer, encoding, done) => {
+        if (encoding === 'binary') {
+          chunks.push(chunk);
+        } else {
+          chunks.push(Buffer.alloc(chunk.length, chunk, encoding));
+        }
+        done();
+      };
+      stream.on('finish', () => {
+        resolve(chunks);
+      });
+      stream.on('error', (err) => {
+        reject(err);
+      });
+    });
+
+    return [stream, promise];
+  },
+  /**
+   * Create a writable stream collecting written text
+   *  and a promise that resolves when it finishes with
+   *  the whole text content.
+   * @function
+   * @returns [Writable, Promise<string>]
+   */
+  toText: function toText(): [Writable, Promise<string>] {
+    const [stream, promise] = StreamTest.toChunks();
+
+    return [stream, promise.then((chunks) => Buffer.concat(chunks).toString())];
+  },
 };
 
 export default StreamTest;
